2d6c666d0c9e6c2e453eff31eb3493faf5a9c050
Glovehead <36669560+Glovehead@users.noreply.github.com>
2018-03-22 15:54:59 +0000

    adding nodejs API

---
+++ api/controllers/guiController.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+exports.getTemperatures = function(req, res){
+
+};
+
+exports.setTemperatures = function(req, res){
+
+};
+
+exports.getTemperatureById = function(req, res){
+
+};
+
+exports.setTemperatureById = function(req, res){
+
+};
+
+exports.getCO2s = function(req, res){
+
+};
+
+exports.setCO2s = function(req, res){
+
+};
+
+exports.getCO2ById = function(req, res){
+
+};
+
+exports.setCO2ById = function(req, res){
+
+};
+
+exports.getHumidities = function(req, res){
+
+};
+
+exports.setHumidities = function(req, res){
+
+};
+
+exports.getHumidityById = function(req, res){
+
+};
+
+exports.setHumidityById = function(req, res){
+
+};
+
+exports.getPressure = function(req, res){
+
+};
+
+exports.setPressure = function(req, res){
+
+};
+
+exports.getPressureById = function(req, res){
+
+};
+
+exports.setPressureById = function(req, res){
+
+};
