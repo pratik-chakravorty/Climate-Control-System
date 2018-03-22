f90b3a36cff16243920b5a2303cd175f92ce189a
Glovehead <36669560+Glovehead@users.noreply.github.com>
2018-03-22 18:16:09 +0000

    added routes to server.js

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser');
+
+  //Mongoose connection
+  //mongoose.Promise = global.Promise;
+  //mongoose.connect('');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/climateControlSystemRoutes');
+routes(app);
 
 var hwController = require('./climateControlModules/hwController');
 //var monitor = require('./climateControlModules/monitor');
 
+
+
 var hw =  new hwController;
 
 hw.buildHWComponentList();
-var readings = hw.getReadings();
-console.log(readings);
 
 app.listen(port);
 
