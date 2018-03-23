ca2c0c7abdb2e7f7ec230f92bd3b64f79613abd7
Glovehead <36669560+Glovehead@users.noreply.github.com>
2018-03-23 18:37:03 +0000

    added message for endpoint not found

--- test/settingsTest.js
+++ test/settingsTest.js
@@ -4,23 +4,45 @@ var settings = new _settings();
 
 describe("Settings tests", function(){
   describe("Update settings"), function(){
-    it("Updates with invalid id", function(){
-      
+    it("Returns error with invalid id", function(){
+      var result = settings.updateSettings("invalid", 100);
+      expect(result).to.equal("Error: Invalid Setting ID");
     });
 
-    it("Updates with null value", function(){});
+    it("Returns error with null value", function(){
+      var result = settings.updateSettings(null, 100);
+      expect(result).to.equal("Error: Invalid Setting ID");
+    });
 
-    it("Updates with temp valid", function(){});
+    it("Updates with temp valid", function(){
+      var result = settings.updateSettings("temp", 30);
+      expect(result).to.equal("OK");
+    });
 
-    it("Updates with temp too low", function(){});
+    it("Returns error with temp too low", function(){
+      var result = settings.updateSettings("temp", -5);
+      expect(result).to.equal("Error: Invalid setting value");
+    });
 
-    it("Updates with temp too high", function(){});
+    it("Returns error with temp too high", function(){
+      var result = settings.updateSettings("temp", 70);
+      expect(result).to.equal("Error: Invalid setting value");
+    });
 
-    it("Updates with co2 valid", function(){});
+    it("Updates with co2 valid", function(){
+      var result = settings.updateSettings("co2", 300);
+      expect(result).to.equal("OK");
+    });
 
-    it("Updates with co2 too low", function(){});
+    it("Updates with co2 too low", function(){
+      var result = settings.updateSettings("co2", -5);
+      expect(result).to.equal("Error: Invalid setting value");
+    });
 
-    it("Updates with co2 too high", function(){});
+    it("Updates with co2 too high", function(){
+      var result = settings.updateSettings("co2", );
+      expect(result).to.equal("Error: Invalid setting value");
+    });
 
     it("Updates with humidity valid", function(){});
 
