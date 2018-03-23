193b01be79e192bfaa289ddb750f9459a9457b7d
Charles BEYER <charles.beyer.pro@gmail.com>
2018-03-23 18:30:18 +0000

    Starting API testing

---
+++ test/apiTests.js
@@ -0,0 +1,57 @@
+var expect = require("chai").expect;
+var request = require("request");
+//var _settings = require("../climateControlModules/settings");
+//var settings = new _settings();
+
+describe("Settings tests", function(){
+  describe("Update settings"), function(){
+    it("Updates with invalid id", function(){
+
+    });
+
+    it("Updates with null value", function(){});
+
+    it("Updates with temp valid", function(){});
+
+    it("Updates with temp too low", function(){});
+
+    it("Updates with temp too high", function(){});
+
+    it("Updates with co2 valid", function(){});
+
+    it("Updates with co2 too low", function(){});
+
+    it("Updates with co2 too high", function(){});
+
+    it("Updates with humidity valid", function(){});
+
+    it("Updates with humidity too low", function(){});
+
+    it("Updates with humidity too high", function(){});
+
+    it("Updates with pressure valid", function(){});
+
+    it("Updates with pressure too low", function(){});
+
+    it("Updates with pressure too high", function(){});
+
+    it("Updates with fan valid", function(){});
+
+    it("Updates with fan too low", function(){});
+
+    it("Updates with fan too high", function(){});
+
+    it("Updates with damper valid", function(){});
+
+    it("Updates with damper too low", function(){});
+
+    it("Updates with damper too high", function(){});
+
+    it("Updates with coil valid", function(){});
+
+    it("Updates with coil too low", function(){});
+
+    it("Updates with coil too high", function(){});
+
+  }
+})
