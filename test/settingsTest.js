940471647b568fc40ed24cd0ac1193722289c279
Glovehead <36669560+Glovehead@users.noreply.github.com>
2018-03-23 18:20:55 +0000

    more settings testing

--- test/settingsTest.js
+++ test/settingsTest.js
@@ -4,9 +4,11 @@ var settings = new _settings();
 
 describe("Settings tests", function(){
   describe("Update settings"), function(){
-    it("Updates with invalid id", function(){});
+    it("Updates with invalid id", function(){
+      
+    });
 
-    it("Updates with null", function(){});
+    it("Updates with null value", function(){});
 
     it("Updates with temp valid", function(){});
 
@@ -14,6 +16,41 @@ describe("Settings tests", function(){
 
     it("Updates with temp too high", function(){});
 
+    it("Updates with co2 valid", function(){});
+
+    it("Updates with co2 too low", function(){});
+
+    it("Updates with co2 too high", function(){});
+
+    it("Updates with humidity valid", function(){});
+
+    it("Updates with humidity too low", function(){});
+
+    it("Updates with humidity too high", function(){});
+
+    it("Updates with pressure valid", function(){});
+
+    it("Updates with pressure too low", function(){});
+
+    it("Updates with pressure too high", function(){});
+
+    it("Updates with fan valid", function(){});
+
+    it("Updates with fan too low", function(){});
+
+    it("Updates with fan too high", function(){});
+
+    it("Updates with damper valid", function(){});
+
+    it("Updates with damper too low", function(){});
+
+    it("Updates with damper too high", function(){});
+
+    it("Updates with coil valid", function(){});
+
+    it("Updates with coil too low", function(){});
+
+    it("Updates with coil too high", function(){});
 
   }
 })
