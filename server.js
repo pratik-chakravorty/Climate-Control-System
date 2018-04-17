06f4600a8cb1e254b9f0f7287d10081ce0d48964
Charles BEYER <charles.beyer.pro@gmail.com>
2018-04-17 14:14:54 +0100

    Back to 3000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8079;
+var port = process.env.PORT || 3000;
 
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
